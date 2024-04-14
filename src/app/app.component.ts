3673bbe38f7dd89fc054868c0e901f772c06db66
Amadeo Bon <amadeosph@outlook.com>
2024-04-14 03:11:09 -0300

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./components/card/card.component";
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    imports: [RouterOutlet, CardComponent, MenuBarComponent, HomeComponent]
+    imports: [RouterOutlet, CardComponent, MenuBarComponent, HomeComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'psn-store';
